04664d71e4d59a48b30819fed3b33c2f81722b8c
aaronskeelsofficial <110141887+aaronskeelsofficial@users.noreply.github.com>
2023-06-01 03:41:42 +0000

    Section 185 CP

---
+++ src/recursion/Fibonacci.java
@@ -0,0 +1,24 @@
+package src.recursion;
+
+public class Fibonacci {
+  private Fibonacci() { 
+  }
+
+  public static void Test_Fib() {
+    System.out.println("fib(0) = " + fib(0));
+    System.out.println("fib(1) = " + fib(1));
+    System.out.println("fib(2) = " + fib(2));
+    System.out.println("fib(3) = " + fib(3));
+    System.out.println("fib(4) = " + fib(4));
+    System.out.println("fib(5) = " + fib(5));
+    System.out.println("fib(6) = " + fib(6));
+  }
+
+  private static int fib(int n) {
+    if (n < 0)
+      return -1;
+    if (n == 0 || n == 1)
+      return n;
+    return fib(n-1) + fib(n-2);
+  }
+}
